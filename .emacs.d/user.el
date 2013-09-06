bc60598ad73301d8ca7ce157008ec22343f16d7f
Nooby <Nooby@gmx.eu>
2013-09-06 03:54:07 +0200

    Add Markdown Mode for Emacs

--- .emacs.d/user.el
+++ .emacs.d/user.el
@@ -68,3 +68,9 @@
 (setq inferior-lisp-program "sbcl")
 
 (add-hook 'nrepl-mode-hook 'paredit-mode)
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
