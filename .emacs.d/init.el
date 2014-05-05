8d4e2d7a282478647976594cf38f4bcf9e0b2bd8
Nooby <dipasqualegiuliano@gmail.com>
2014-05-05 03:43:08 +0200

    Emacs Solarized Colorsheme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,18 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+       '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (defvar my-packages '(starter-kit
-		      starter-kit-lisp
-		      starter-kit-bindings
-		      starter-kit-eshell
-		      clojure-mode
-		      clojure-test-mode
-		      cider))
+          starter-kit-lisp
+          starter-kit-bindings
+          starter-kit-eshell
+          clojure-mode
+          clojure-test-mode
+          cider
+          color-theme-solarized))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+(load-theme 'solarized-dark t)
