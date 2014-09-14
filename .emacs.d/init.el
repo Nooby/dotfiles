0d93de3e3c37d41ed1650bd4767a6649b8209750
Nooby <dipasqualegiuliano@gmail.com>
2014-09-14 16:18:44 +0200

    Updated Emacs Config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,11 +5,11 @@
 
 (defvar my-packages '(starter-kit
           starter-kit-lisp
-          starter-kit-bindings
+          ;starter-kit-bindings
           starter-kit-eshell
-          clojure-mode
-          clojure-test-mode
-          cider
+          ;clojure-mode
+          ;clojure-test-mode
+          ;cider
           color-theme-solarized))
 
 (dolist (p my-packages)
