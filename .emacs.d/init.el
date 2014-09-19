e7400199cff653ededa41e2aba2077cbd5adfb0e
Giuliano Di Pasquale <dipasquale@mir.de>
2014-09-19 20:56:52 +0200

    Projectile Added to Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 (package-initialize)
 
 (defvar my-packages '(better-defaults
+          projectile
 		      clojure-mode
 		      clojure-test-mode
 		      cider))
@@ -11,3 +12,16 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
