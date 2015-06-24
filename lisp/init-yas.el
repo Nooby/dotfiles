b62807f131a8676b03b10b91d89d10922ac8a653
Nooby <dipasqualegiuliano@gmail.com>
2015-06-25 01:27:55 +0200

    Added the Yasnippet Root Directory to Config

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -1,6 +1,9 @@
 (ensure-packages-installed '(yasnippet))
 
 (require 'yasnippet)
+
+(add-to-list 'yas/root-directory (expand-file-name "snippet" user-emacs-directory))
+(yas/initialize)
 (yas-global-mode 1)
 
 (provide 'init-yas)
