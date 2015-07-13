593b1fa0e8c0aaf7aea4d9f7e2cacab1d120ab9a
Giuliano Di Pasquale <dipasqualegiuliano@gmail.com>
2015-07-13 15:48:15 +0200

    Remove DOS Line Endings from Files

--- lisp/init-base-settings.el
+++ lisp/init-base-settings.el
@@ -54,4 +54,14 @@
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
            (flet ((process-list ())) ad-do-it))
 
+(defun no-dos-please-were-unixish ()
+  (let ((coding-str (symbol-name buffer-file-coding-system)))
+    (when (string-match "-dos$" coding-str)
+      (setq coding-str
+	    (concat (substring coding-str 0 (match-beginning 0)) "-unix"))
+      (message "CODING: %s" coding-str)
+	       (set-buffer-file-coding-system (intern coding-str)))))
+
+(add-hook 'find-file-hooks 'no-dos-please-were-unixish)
+
 (provide 'init-base-settings)
