8aec41bdaa866245569ed00a7d1d7dde654647fd
Nooby <dipasqualegiuliano@gmail.com>
2015-08-25 17:54:27 +0200

    Fixed Yasnippet

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -3,7 +3,11 @@
 (require 'yasnippet)
 
 (add-to-list 'yas/root-directory (expand-file-name "snippet" user-emacs-directory))
+(require 'yasnippet)
 (yas/initialize)
 (yas-global-mode 1)
+
+;;(global-set-key (kbd "<tab>") 'yas-next-field-or-maybe-expand)
+(define-key yas-minor-mode-map (kbd "<M-tab>") 'yas-expand)
 
 (provide 'init-yas)
