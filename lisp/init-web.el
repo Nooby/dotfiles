ae6cc4eca9ab60579548c79b946ef43d56b95df8
Giuliano Di Pasquale <dipasqualegiuliano@gmail.com>
2016-01-26 13:38:59 +0100

    Added PHP to WebMode

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.gotmpl$" . web-mode))
