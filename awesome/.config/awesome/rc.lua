ca09dfbdf987e287491179b1a3a351a61323baad
Giuliano Di Pasquale <giuliano@paxlife.aero>
2017-02-17 10:15:04 +0100

    Various changes

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -153,7 +153,8 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 mytextclock = awful.widget.textclock()
 
 -- Create Battery Widget
-local battery = battery_widget:new({})
+local battery1 = battery_widget:new({})
+local battery2 = battery_widget:new({battery = "BAT1"})
 
 -- Create a wibox for each screen and add it
 mywibox = {}
@@ -234,7 +235,8 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(battery.widget)
+    if s == 1 then right_layout:add(battery1.widget) end
+    if s == 1 then right_layout:add(battery2.widget) end
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
@@ -522,4 +524,3 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 --     end
 -- end
 -- }}}
-
--- emacs/.emacs.d/lisp/lang-markdown.el
+++ emacs/.emacs.d/lisp/lang-markdown.el
@@ -3,6 +3,14 @@
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode)))
+         ("\\.markdown\\'" . markdown-mode))
+  :config
+  (custom-set-variables '(markdown-command "/usr/bin/pandoc")))
+
+(use-package apib-mode
+  :ensure t
+  :mode (("\\.apib\\'" . apib-mode))
+  :config
+  (custom-set-variables '(markdown-command "/usr/bin/pandoc")))
 
 (provide 'lang-markdown)
