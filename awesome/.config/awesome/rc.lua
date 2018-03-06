6a3041fb1d18a714903a3c46cdbb0b2118a986bd
Giuliano Di Pasquale <giuliano@paxlife.aero>
2018-03-06 17:23:23 +0100

    Changed Awesome Theme
    - Changed Theme to Zenburn
    - Reduced Icon Size for Notifications
    - Removed some Tiling modes

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -39,9 +39,10 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
 
 beautiful.wallpaper = "/home/giuliano/Pictures/Wallpaper/Cyberpunk/103_SbNm3Sr.jpg"
+naughty.config.defaults.icon_size = 100
 
 
 -- This is used later as the default terminal and editor to run.
@@ -58,18 +59,18 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
+    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
+    -- awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
