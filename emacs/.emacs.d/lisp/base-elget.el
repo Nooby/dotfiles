e8e4e5af1a25b182c5aa712ead32a02f3132a653
Giuliano Di Pasquale <giuliano@paxlife.aero>
2019-04-26 19:21:56 +0200

    [EMACS] Add elget to emacs config

---
+++ emacs/.emacs.d/lisp/base-elget.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
