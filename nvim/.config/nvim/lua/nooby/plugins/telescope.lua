8cb932db242dda0787cbaf2986af3173bc9477c0
Giuliano Di Pasquale <giuliano@dipasquale.dev>
2024-04-23 18:52:59 +0200

    Add binding for telescope git files

--- nvim/.config/nvim/lua/nooby/plugins/telescope.lua
+++ nvim/.config/nvim/lua/nooby/plugins/telescope.lua
@@ -69,7 +69,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch Git [F]iles' })
     vim.keymap.set('n', '<leader>n', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
