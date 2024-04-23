92582822a059a17407d5efa3541de0f86f04beae
Giuliano Di Pasquale <giuliano@dipasquale.dev>
2024-04-23 09:13:49 +0200

    Add gopls LSP config

--- nvim/.config/nvim/lua/nooby/plugins/lsp.lua
+++ nvim/.config/nvim/lua/nooby/plugins/lsp.lua
@@ -145,7 +145,21 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      gopls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            codelenses = {
+              gc_details = true,
+              tidy = true,
+              upgrade_dependency = true,
+            },
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      },
       pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
